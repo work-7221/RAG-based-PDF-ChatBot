0d2d5ea67da5a1a122681b454e81f15311acf209
work-7221 <work.rohankm7221@gmail.com>
2026-06-26 12:47:49 +0530

    The system is successfully able to find collections of already passed files in persistent DB and create new ones if the file was not given

--- Functions/storing_function_VDB.py
+++ Functions/storing_function_VDB.py
@@ -1,16 +1,37 @@
 import chromadb
 import os
+from Functions.embedding_generation import embedder
+from Functions.chunker import chunk_text
 
+file_path = ""
 
-def storing_into_vectorDB(embeddings, chunks, file_path):
+def check_for_collection(file_path):
     client = chromadb.PersistentClient(path = "Chroma_Database")
-
-    collection = client.get_or_create_collection(f"{os.path.basename(file_path)}_collection")
+    all_collections = client.list_collections()
+    for collection in all_collections:
+        if (collection.name == f"{os.path.basename(file_path)}_collection"):
+            return True
     
-    collection.add(
-    documents=chunks,
-    embeddings=embeddings.tolist(),  # Converts your NumPy array to a Python list
-    ids=[f"chunk_{i}" for i in range(len(chunks))]
-    )
+    file_path = file_path
+
+    return False
+
+def storing_into_vectorDB(condition,
+                          file_path,
+                          ):
+    chunks = chunk_text(file_path)
+    embeddings = embedder(file_path)
+    
+    client = chromadb.PersistentClient(path = "Chroma_Database")
+    if (condition == False):
+        collection = client.create_collection(f"{os.path.basename(file_path)}_collection")
+    
+        collection.add(
+        documents=chunks,
+        embeddings=embeddings.tolist(),  # Converts your NumPy array to a Python list
+        ids=[f"chunk_{i}" for i in range(len(chunks))]
+        )
+    else:
+        collection = client.get_collection(f"{os.path.basename(file_path)}_collection")
 
     return collection
--- Main.py
+++ Main.py
@@ -2,42 +2,48 @@ from Functions.PDF_Loader import reader_function
 from Functions.chunker import chunk_text
 from Functions.embedding_generation import embedder
 from Functions.storing_function_VDB import storing_into_vectorDB
+from Functions.storing_function_VDB import check_for_collection
 from Functions.Querior_Processor import query_processor
 from Functions.Ollama_LLM import generate_answer
 from Functions.prompt_builder import build_prompt
 
 path = input("Enter the path: ")
 pages = reader_function(path)
-
+condition = True
 
 print("="*120)
 print("✅ All imports successful!")
 print("="*120)
 
-
-retrieved_information = reader_function("PDFs/research_upperBody.pdf")
-chunks_from_retrieved_information = chunk_text(retrieved_information)
-
-
-print("✅ The document's content has been retrieved")
-print("="*120)
-
-print(retrieved_information)
-print("="*120)
-print("✅ The document's content has been chunked down")
-print("="*120)
-
-print(chunks_from_retrieved_information)
-print("="*120)
+if not (check_for_collection(path)):
+    condition = False
+    print("The file's context is not there in the database, we will add the context to the databse.")
+    retrieved_information = reader_function(path)
+    chunks_from_retrieved_information = chunk_text(retrieved_information)
 
 
-print("✅ The document's chunks has been embedded")
-embeddings = embedder(chunks_from_retrieved_information)
-print("="*120)
-print("✅ The embeddings has been stored in vector db")
+    print("✅ The document's content has been retrieved")
+    print("="*120)
 
-vector_db_collection = (storing_into_vectorDB(embeddings, chunks_from_retrieved_information, path))
-print("="*120)
+    print(retrieved_information)
+    print("="*120)
+    print("✅ The document's content has been chunked down")
+    print("="*120)
+
+    print(chunks_from_retrieved_information)
+    print("="*120)
+
+
+    print("✅ The document's chunks has been embedded")
+    embeddings = embedder(chunks_from_retrieved_information)
+    print("="*120)
+    print("✅ The embeddings has been stored in vector db")
+
+    vector_db_collection = storing_into_vectorDB(condition, path)
+    print("="*120)
+else:
+    vector_db_collection = storing_into_vectorDB(condition, file_path = path)
+
 
 processed_query = query_processor()
 print("✅ A query has been asked from the user and has been processed.")
@@ -55,5 +61,5 @@ answer_to_query = generate_answer(build_prompt(processed_query[1], context))
 print("✅ The query has been send to the LLM with the context from the provided knowledge base.")
 print(answer_to_query)
 print("="*120)
-print("✅ A response from the Local LLM has been geenrated")
+print("✅ A response from the Local LLM has been generated")
 print("="*120)
